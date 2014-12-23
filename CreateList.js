914efbead32a4ebae6381889c9b53e081fe6e849
Spencer Robinson <theredberrys@gmail.com>
2014-12-22 21:15:05 -0500

    Added CreateList.js

---
+++ CreateList.js
@@ -0,0 +1,31 @@
+var availableCards = [
+	"I would be ungrateful if...",
+	"Member of the bishopric falls asleep",
+	"Tender Mercies",
+	"Odd use of the word 'Even'",
+	"Someone getting up after the meeting should be over",
+	"Mad dash for first place",
+	"Unaccompanied minor",
+	"Politics",
+	"Name dropper",
+	"...with every fiber of my being...",
+	"The Arrangement (I'll go up if you go up)",
+	"Too much information",
+	"In the name of Thy Son...",
+	"Accompanied minor (with parent whispering)",
+	"Bad Joke (congregation doesn't laugh)",
+	"The Regular",
+	"Moment of Silence (over 30 seconds)",
+	"Movie Comparison",
+	"On my mission...",
+	"The Ugly Cry",
+	"The Long Talker (over ten minutes)",
+	"Not a member of the ward",
+	"Good Joke (congregation laughs)",
+	"Travelogue",
+	"Medical Problems",
+	"Someone talks in a foreign language"
+];
+
+var testDiv = document.getElementById("test");
+testDiv.innerHTML = availableCards.join("<br/>");
