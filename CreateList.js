326733185ac021522707ea34a3117ea587f557d2
Spencer Robinson <theredberrys@gmail.com>
2014-12-28 15:39:15 -0500

    Added versions for sacrament meeting, relief society, and Sunday school.

--- CreateList.js
+++ CreateList.js
@@ -1,38 +1,3 @@
-var availableCards = [
-	"I would be ungrateful if...",
-	"Member of the bishopric falls asleep",
-	"Tender Mercies",
-	"Odd use of the word 'Even'",
-	"Someone gets up after the meeting should end",
-	"Mad dash for first place",
-	"Unaccompanied minor",
-	"Politics",
-	"Name dropper",
-	"...With every fiber of my being...",
-	"The Arrangement (I'll go up if you go up)",
-	"Too much information",
-	"In the name of Thy Son...",
-	"Accompanied minor (with parent whispering)",
-	"Bad Joke (congregation doesn't laugh)",
-	"The Regular",
-	"Moment of Silence (over 30 seconds)",
-	"Movie Comparison",
-	"On my mission...",
-	"The Ugly Cry",
-	"The Long Talker (over ten minutes)",
-	"Not a member of the ward",
-	"Good Joke (congregation laughs)",
-	"Travelogue",
-	"Medical Problems",
-	"Someone talks in a foreign language",
-	"I don't know where I would be...",
-	"I don't know why I'm up here...",
-	"Three Nephites",
-	"Entire testimony without mentioning Jesus",
-	"Bruce R. McConkie Quote",
-	"A child cries for 5 minutes"
-];
-
 var usedCards = [];
 
 for (i = 0; i < 24; i++) {
